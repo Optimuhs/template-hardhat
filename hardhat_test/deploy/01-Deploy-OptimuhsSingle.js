e141bf4ba7f2916098e3d2b99b467818785b5d07
optimuhs <optimuhs7@gmail.com>
2023-04-07 16:00:11 -0500

    fix files

--- hardhat_test/deploy/01-Deploy-OptimuhsSingle.js
+++ hardhat_test/deploy/01-Deploy-OptimuhsSingle.js
@@ -10,27 +10,6 @@ module.exports = async({getNamedAccounts, deployments}) => {
     console.log(`Deploying optimuhs single mint contract on ${network.name}.`)
     const optimuhsDep = await deploy("OptimuhsSingle", {
         from: deployer,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        args: [ethers.utils.parseEther("0.001"), 3, 10000], // mint price, amount per, total supply
-=======
-        args: [ethers.utils.parseEther("0.001"), 10, 10000], // mint price, amount per, total supply
->>>>>>> a677a62bc034960a1b779a32089563fc6fb84cd2
-=======
-        args: [ethers.utils.parseEther("0.001"), 10, 10000], // mint price, amount per, total supply
->>>>>>> a677a62bc034960a1b779a32089563fc6fb84cd2
-=======
-        args: [ethers.utils.parseEther("0.001"), 10, 10000], // mint price, amount per, total supply
->>>>>>> a677a62bc034960a1b779a32089563fc6fb84cd2
-=======
-        args: [ethers.utils.parseEther("0.001"), 10, 10000], // mint price, amount per, total supply
->>>>>>> a677a62bc034960a1b779a32089563fc6fb84cd2
-=======
-        args: [ethers.utils.parseEther("0.001"), 10, 10000], // mint price, amount per, total supply
->>>>>>> a677a62bc034960a1b779a32089563fc6fb84cd2
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
